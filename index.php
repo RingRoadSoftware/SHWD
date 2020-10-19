e8a9fcd7b56a9cb3b819b3315bb775cbda4d85f9
xero <marius.scutea.00@gmail.com>
2020-10-19 18:18:50 +1030

    HTML & CSS WIP
    Cannot remove horizontal scroll bar from index.php

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Welcome to Steven Hawking Web Dev Shop</title>
+		<title>RR Audio Solutions</title>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	</head>
 	<body style="min-width: 980px;">
 		<div class="homepageheader" style="position: relative;">
-			<div style="float: left; margin: 5px 0px 0px 23px;">
-				<a href="index.php">
-					<img style=" height: 75px; width: 130px;" src="image/LOGO-HERE.png"> 
+
+		<a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img>
+			<nav>
+				<ul>
+				<li> <a href="products.php">Products</a></li>
+				<li> <a href="cart.php">Cart</a></li>
+				<li> <a href="about.php">About</a></li>
+				</ul>		
+			</nav>
 				</a>
 			</div>
 		</div>
@@ -18,44 +23,48 @@
 
 		<div class="home-welcome">
 			<div class="home-welcome-text" style="height: 380px; ">
-				<h1 style="margin: 0px;">Welcome To Our Shop</h1>
+				<h1><img style="width: 45%;" src="./images/ringroad logo.png" ></h1>
 				<h2>Great Deals on Headphones</h2>
+			</div>
+			<div class="sale-banner"> 
+				<a href="sale.php"><img style="display: block; margin: 0 auto;" src="./images/sale banner.jpg">
+			</a>
 			</div>
 		</div>
 
-		<!-- we should implement a php function to pull the pPage value and picture from the database instead of hardcoding it in. 
+		<!-- we should implement a php function to pull the page value and picture from the database instead of hardcoding it in. 
 		So this would just be similar code to that of Practical 3's index.php which reads the tasks from the database, but instead that 
 		reads products.sql and embed the image and link in the webpage.-->
 		<div class="home-prodlist">
 			<div>
-				<h3 style="text-align: center;">Featured Products</h3>
+				<h3 style="text-align: center; margin-top:100px;">Featured Products</h3>
 			</div>
 			<div style="padding: 20px 30px; width: 85%; margin: 0 auto;">
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
-						<div class="home-product-img"><a href="product-pages/product1.php">
-							<img src="./image/product/saree/hijab 1.png" class="home-products"></a>
+						<div class="home-product-img"><a href="product-pages/hd800s.php">
+							<img src="./images/HD800S.jpg" class="home-products"></a>
 						</div>
 					</li>
 				</ul>
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
-						<div class="home-product-img"><a href="product-pages/product2.php">
-							<img src="./image/product/beauty/toiletries.png" class="home-products"></a>
+						<div class="home-product-img"><a href="product-pages/ananda.php">
+							<img src="./images/ANANDA.jpg" class="home-products"></a>
 						</div>
 					</li>
 				</ul>
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
-						<div class="home-product-img"><a href="product-pages/product3.php">
-							<img src="./image/product/footwear/footwear1.png" class="home-products"></a>
+						<div class="home-product-img"><a href="product-pages/ananda.php">
+							<img src="./images/HD6XX.jpg" class="home-products"></a>
 						</div>
 					</li>
 				</ul>
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
-						<div class="home-product-img"><a href="product-pages/product4.php">
-							<img src="./image/product/saree/tshirts1.png" class="home-products"></a>
+						<div class="home-product-img"><a href="product-pages/he4xx.php">
+							<img src="./images/HE4XX.jpg" class="home-products"></a>
 						</div>
 					</li>
 				</ul>
