36ee5e007b0867603db6e05bbac6a21dab6327ee
rainbowWren <72905517+rainbowWren@users.noreply.github.com>
2020-10-23 10:08:32 +1030

    updated with the beginning of a category layout

--- products.php
+++ products.php
@@ -1,1 +1,39 @@
-
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>RR Audio Solutions</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body style="min-width: 980px;">
+		<div class="homepageheader" style="position: relative;">
+		<a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img>
+			<nav>
+				<ul>
+				<li> <a href="products.php">Products</a></li>
+				<li> <a href="cart.php">Cart</a></li>
+				<li> <a href="about.php">About</a></li>
+				</ul>		
+			</nav>
+				</a>
+			</div>
+		</div>
+		<div class="home-welcome">
+			<div class="home-welcome-text" style="height: 380px; ">
+				<h1><img style="width: 45%;" src="./images/ringroad logo.png" ></h1>
+				<h2>Great Deals on Headphones</h2>
+			</div>
+			<div class="product-categories"> 
+        <nav>
+				<ul>
+				<li> <a href="openback.php">Open Back Headphones</a></li>
+				<li> <a href="onear.php">On Ear Headphones</a></li>
+				<li> <a href="inear.php">In Ear </a></li>
+				</ul>		
+			</nav>
+			</a>
+			</div>
+		</div>
+	
+	</body>
+</html>
