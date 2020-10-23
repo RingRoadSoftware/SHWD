4ace1a10ff5d2920c4297899eb74721f5aff2311
rainbowWren <72905517+rainbowWren@users.noreply.github.com>
2020-10-23 12:41:10 +1030

    added the header and footer

--- checkout-payment.php
+++ checkout-payment.php
@@ -5,7 +5,20 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
+
     <body>
+	    
+	    div class="homepageheader" style="position: relative;">
+		<a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img>
+			<nav>
+				<ul>
+				<li> <a href="products.php">Products</a></li>
+				<li> <a href="cart.php">Cart</a></li>
+				<li> <a href="about.php">About</a></li>
+				</ul>		
+			</nav>
+				</a>
+			</div>
         <h1>Payment Information</h1>
         <form action="add-payment.php" method="POST">
             <label for="card-name">Card Name</label>
@@ -18,5 +31,16 @@
             <input type="text" name="cvv" id="cvv">
             <input type="submit" class="submit-button" value="Proceed to Shipping">
         </form>
+	    <div class="footer">
+		<a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img>
+			<nav>
+				<ul>
+				<li> <a href="social.php">Socials</a></li>
+				<li> <a href="copyright.php">Copyright and Privacy</a></li>
+				<li> <a href="contact.php">Contact Us</a></li>
+				</ul>		
+			</nav>
+				</a>
+			</div>
     </body>
 </html>
