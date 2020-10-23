5b21d39ccd8d239af8dda0018fcdafe040d53203
xero <marius.scutea.00@gmail.com>
2020-10-23 22:37:45 +1030

    Update HD800S.php

--- product-pages/HD800S.php
+++ product-pages/HD800S.php
@@ -1,12 +1,51 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<title>Sennheiser HD800S</title>
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body style="min-width: 980px;">
+		<div class="homepageheader" style="position: relative;">
 
-<head>
-    <title
-</head>
+		<a href="index.php"><img src="../images/Ringroad Logo.png" id="logo"></img>
+			<nav>
+				<ul>
+				<li> <a href="products.php">Products</a></li>
+				<li> <a href="cart.php">Cart</a></li>
+				<li> <a href="about.php">About</a></li>
+				</ul>		
+			</nav>
+				</a>
+			</div>
+		</div>
 
-<body>
-<body>
 
-
+		<div class="home-welcome">
+			<div class="home-welcome-text" style="height: 380px; ">
+				<h1><img style="width: 45%;" src="../images/ringroad logo.png" ></h1>
+				<h2>Sennheiser HD800S</h2>
+                <div class="image-content"  style="
+                    margin: 0 auto;
+                    width: 50%;
+                    padding: 10px;
+                ">
+                <img src="../images/HD800S.jpg" style="  display: block;
+                margin-left: auto;
+                margin-right: auto;
+                ">
+                </div>
+			</div>
+		<div class="footer">
+			<a href="index.php"><img src="../images/Ringroad Logo.png" id="logo"></img>
+				<nav>
+					<ul>
+					<li> <a href="social.php">Socials</a></li>
+					<li> <a href="copyright.php">Copyright and Privacy</a></li>
+					<li> <a href="contact.php">Contact Us</a></li>
+					</ul>		
+				</nav>
+				</a>
+		</div>
+	</body>
 </html>
