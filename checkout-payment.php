52f1d87ecf33fcca279c2bf58338cebc742e619f
Pungga <65751800+Pungga@users.noreply.github.com>
2020-10-23 15:58:24 +1030

    checkout stylised + added security

--- checkout-payment.php
+++ checkout-payment.php
@@ -5,42 +5,29 @@
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
-
     <body>
-	    
-	    div class="homepageheader" style="position: relative;">
-		<a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img>
-			<nav>
-				<ul>
-				<li> <a href="products.php">Products</a></li>
-				<li> <a href="cart.php">Cart</a></li>
-				<li> <a href="about.php">About</a></li>
-				</ul>		
-			</nav>
-				</a>
-			</div>
-        <h1>Payment Information</h1>
-        <form action="add-payment.php" method="POST">
+        <div class="homepageheader" style="position: relative;">
+		    <a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img></a>
+        </div>
+        <div class="form-body">
+        <h1 class="checkout-header">Enter payment details</h1>
+        <form action="add-payment.php" method="POST" class="form">
             <label for="card-name">Card Name</label>
+            <br>
             <input type="text" name="card-name" id="card-name">
+            <br>
             <label for="card-number">Card Number</label>
-            <input type="text" name="card-number" id="card-number">
-            <label for="expiration-date">Expiration Date</label>
-            <input type="text" name="expiration-date" id="expiration-date">
-            <label for="cvv">CVV</label>
-            <input type="text" name="cvv" id="cvv">
+            <br>
+            <input type="text" name="card-number" id="card-number" minlength="16" maxlength="16">
+            <br>
+            <label for="expiration-date" id="expiration-date-label">Date</label>
+            <label for="cvv" id="cvv">CVV</label>
+            <br>
+            <input type="text" name="expiration-date" id="expiration-date" minlength="5" maxlength="5">
+            <input type="text" name="cvv" id="cvv" minlength="3" maxlength="3">
+            <br>
             <input type="submit" class="submit-button" value="Proceed to Shipping">
         </form>
-	    <div class="footer">
-		<a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img>
-			<nav>
-				<ul>
-				<li> <a href="social.php">Socials</a></li>
-				<li> <a href="copyright.php">Copyright and Privacy</a></li>
-				<li> <a href="contact.php">Contact Us</a></li>
-				</ul>		
-			</nav>
-				</a>
-			</div>
+        </div>
     </body>
 </html>
--- checkout-shipping.php
+++ checkout-shipping.php
@@ -6,43 +6,35 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
     <body>
-	 <div class="homepageheader" style="position: relative;">
-		<a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img>
-			<nav>
-				<ul>
-				<li> <a href="products.php">Products</a></li>
-				<li> <a href="cart.php">Cart</a></li>
-				<li> <a href="about.php">About</a></li>
-				</ul>		
-			</nav>
-		</a>
-	</div>   
-        <h1>Shipping Information</h1>
-        <form action="add-shipping.php" method="POST">
-            <label for="first-name">First Name</label>
-            <input type="text" name="first-name" id="first-name">
+        <div class="homepageheader" style="position: relative;">
+		    <a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img></a>
+        </div>
+        <div class="form-body">
+        <h1 class="checkout-header">Shipping Information</h1>
+        <form action="add-shipping.php" method="POST" class="form">
+            <label for="first-name" id="first-name-label">First Name</label>
             <label for="last-name">Last Name</label>
+            <br>
+            <input type="text" name="first-name" id="first-name">
             <input type="text" name="last-name" id="last-name">
+            <br>
             <label for="mobile-number">Mobile Number</label>
-            <input type="text" name="mobile-number" id="mobile-number">
+            <br>
+            <p>+61</p>
+            <input type="text" name="mobile-number" id="mobile-number" minlength="10" maxlength="10">
+            <br>
             <label for="address">Address</label>
+            <br>
             <input type="text" name="address" id="address">
-            <label for="zipcode">Zipcode</label>
-            <input type="text" name="zipcode" id="zipcode">
+            <br>
+            <label for="zipcode" id="zipcode-label">Zipcode</label>
             <label for="company">Company</label>
+            <br>
+            <input type="text" name="zipcode" id="zipcode" minlength="4" maxlength="4">
             <input type="text" name="company" id="company" placeholder="Optional">
+            <br>
             <input type="submit" class="submit-button" value="Confirm Order">
         </form>
-	<div class="footer">
-		<a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img>
-			<nav>
-				<ul>
-				<li> <a href="social.php">Socials</a></li>
-				<li> <a href="copyright.php">Copyright and Privacy</a></li>
-				<li> <a href="contact.php">Contact Us</a></li>
-				</ul>		
-			</nav>
-		</a>
-	</div>
+        </div>
     </body>
 </html>
