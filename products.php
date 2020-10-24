c6d873b50896695269f6c52de9a906766a7577a2
skeptasnut <54111645+skeptasnut@users.noreply.github.com>
2020-10-24 15:15:28 +1030

    eddfd
    dd

--- products.php
+++ products.php
@@ -23,17 +23,141 @@
 				<h1><img style="width: 45%;" src="./images/ringroad logo.png" ></h1>
 				<h2>Great Deals on Headphones</h2>
 			</div>
-			<div class="product-categories"> 
-        <nav>
-				<ul>
-				<li> <a href="openback.php">Open Back Headphones</a></li>
-				<li> <a href="onear.php">On Ear Headphones</a></li>
-				<li> <a href="inear.php">In Ear </a></li>
-				</ul>		
-			</nav>
-			</a>
-			</div>
+		
+			<div class="home-prodlist">
+		<div>
+			<h3 style="text-align: center; margin-top:100px;">All Products</h3>
 		</div>
+		<div style="padding: 20px 30px; width: 85%; margin: 0 auto;">
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px;">
+					<div class="home-product-img">
+						<img src="./images/HD800S.jpg" class="home-products">
+						<a href="product-pages/hd800s.php" class="product-link">
+							<div class="overlay">
+								<div class="overlay-text">Sennheiser HD-800S - $2499</div>
+							</div>
+						</a>
+					</div>
+				</li>
+			</ul>
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px;">
+					<div class="home-product-img">
+						<img src="./images/ANANDA.jpg" class="home-products">
+						<a href="product-pages/ananda.php" class="product-link">
+							<div class="overlay">
+								<div class="overlay-text">HiFiMAN Ananda - $1199</div>
+							</div>
+					</div>
+					</a>
+				</li>
+			</ul>
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px;">
+					<div class="home-product-img">
+						<img src="./images/HD6XX.jpg" class="home-products">
+						<a href="product-pages/hd6xx.php" class="product-link">
+							<div class="overlay">
+								<div class="overlay-text">Sennheiser HD-6XX - $280</div>
+							</div>
+					</div>
+
+				</li>
+			</ul>
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px;">
+					<div class="home-product-img">
+						<img src="./images/HE4XX.jpg" class="home-products">
+						<a href="product-pages/he4xx.php">
+							<div class="overlay">
+								<div class="overlay-text">HiFiMAN HE-4XX - $200</div>
+							</div>
+					</div>
+
+				</li>
+			</ul>
+
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px;">
+					<div class="home-product-img">
+						<img src="./images/L700.jpg" class="home-products">
+						<a href="product-pages/L700.php">
+							<div class="overlay">
+								<div class="overlay-text">STAX L700 - $2199</div>
+							</div>
+					</div>
+
+				</li>
+			</ul>
+
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px;">
+					<div class="home-product-img">
+						<img src="./images/LCD3.jpg" class="home-products">
+						<a href="product-pages/LCD3.php">
+							<div class="overlay">
+								<div class="overlay-text">Audeze LCD3 - $3049</div>
+							</div>
+					</div>
+
+				</li>
+			</ul>
+
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px;">
+					<div class="home-product-img">
+						<img src="./images/SR80E.jpg" class="home-products">
+						<a href="product-pages/SR80E.php">
+							<div class="overlay">
+								<div class="overlay-text">Grado SR80E - $169</div>
+							</div>
+					</div>
+
+				</li>
+			</ul>
+
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px;">
+					<div class="home-product-img">
+						<img src="./images/ATHM40X.jpg" class="home-products">
+						<a href="product-pages/M40X.php">
+							<div class="overlay">
+								<div class="overlay-text">Audio Technica M40X - $169</div>
+							</div>
+					</div>
+
+				</li>
+			</ul>
+
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px;">
+					<div class="home-product-img">
+						<img src="./images/WH1000XM4.jpg" class="home-products">
+						<a href="product-pages/sale.php">
+							<div class="overlay">
+								<div class="overlay-text">Sony WH100XM4 - $400</div>
+							</div>
+					</div>
+
+				</li>
+			</ul>
+
+			<ul style="float: left;">
+				<li style="float: left; padding: 25px; padding-bottom: 200px;">
+					<div class="home-product-img">
+						<img src="./images/CLEAR.jpg" class="home-products">
+						<a href="product-pages/CLEAR.php">
+							<div class="overlay">
+								<div class="overlay-text">Focal Clear - $1199</div>
+							</div>
+					</div>
+
+				</li>
+			</ul>
+		</div>
+
+			
 	<div class="footer">
 		<a href="index.php"><img src="./images/Ringroad Logo.png" id="logo"></img>
 			<nav>
