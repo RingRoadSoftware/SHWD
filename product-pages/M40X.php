afa949234d5fd1517b65a3bad3ad5579b4e21352
rainbowWren <72905517+rainbowWren@users.noreply.github.com>
2020-10-24 16:36:28 +1030

    fixed broken image link

--- product-pages/M40X.php
+++ product-pages/M40X.php
@@ -24,7 +24,7 @@
     <div class="home-welcome-text" style="height: 380px; ">
         <h1>Audio Technica ATH-M40X</h1>
         <div class="image-content" style="margin: 0 auto; width: 85%; height: 100%; padding: 10px;">
-            <img src="../images/M40X.jpg" align="left" style="display: block; margin-left: auto; margin-right: auto;">
+            <img src="../images/ATHM40X.jpg" align="left" style="display: block; margin-left: auto; margin-right: auto;">
             <div class="info" style="width: 65%; margin-left: 9%;">
                 <p style="color: black; font-size: 20px; text-align: justify;">The M-Series ATH-M40x professional
                     monitor headphones are tuned flat for incredibly accurate audio monitoring across an extended
