359f3d9f8982628e26c6e69d47d322e9486c612b
xero <marius.scutea.00@gmail.com>
2020-10-24 14:48:06 +1030

    10 Products Added

---
+++ product-pages/M40X.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ATH-M40X</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body style="min-width: 980px;">
+    <div class="homepageheader" style="position: relative;">
+
+        <a href="../index.php"><img src="../images/Ringroad Logo.png" id="logo"></img>
+            <nav>
+                <ul>
+                    <li> <a href="products.php">Products</a></li>
+                    <li> <a href="cart.php">Cart</a></li>
+                    <li> <a href="about.php">About</a></li>
+                </ul>
+            </nav>
+        </a>
+    </div>
+    </div>
+    <div class="home-welcome-text" style="height: 380px; ">
+        <h1>Audio Technica ATH-M40X</h1>
+        <div class="image-content" style="margin: 0 auto; width: 85%; height: 100%; padding: 10px;">
+            <img src="../images/M40X.jpg" align="left" style="display: block; margin-left: auto; margin-right: auto;">
+            <div class="info" style="width: 65%; margin-left: 9%;">
+                <p style="color: black; font-size: 20px; text-align: justify;">The M-Series ATH-M40x professional
+                    monitor headphones are tuned flat for incredibly accurate audio monitoring across an extended
+                    frequency range. Your studio experience is enhanced with superior sound isolation and swiveling
+                    earcups for convenient one-ear monitoring. Engineered with pro-grade materials and robust
+                    construction, the M40x excels in professional studio tracking and mixing, as well as DJ monitoring.
+                </p>
+            </div>
+            <div class="purchase-info">
+                <form action="TODO: REPLACE WITH ADD TO CART FORM">
+                    <p style="color: black; font-size: 25px;">$169</p>
+                    <input type="submit" value="Add to cart" id="cart-button"></input>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <nav>
+            <ul>
+                <li> <a href="social.php">Socials</a></li>
+                <li> <a href="copyright.php">Copyright and Privacy</a></li>
+                <li> <a href="contact.php">Contact Us</a></li>
+            </ul>
+        </nav>
+        </a>
+    </div>
+</body>
+
+</html>
